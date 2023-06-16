8e91c077e66e173f5c185376bbb457ffe24991b4
pavel <pavelpichugin28@gmail.com>
2023-06-16 22:17:53 +0400

    resolved errors and added static.cpp file

---
+++ static.cpp
@@ -0,0 +1,23 @@
+#include "static.h"
+
+Static:: Static() {}
+
+Static::~Static() {}
+
+void Static::Init(std::string textureName, sf::Vector2f position) {
+
+	m_position = position;
+
+	m_texture.loadFromFile(textureName.c_str());
+
+	//shape.setTexture(&m_texture);
+	m_sprite_size = sf::Vector2i(80, 16);
+
+	m_sprite.setTexture(m_texture);
+	m_sprite.setPosition(m_position);
+}
+
+sf::Sprite Static::getSprite()
+{
+	return m_sprite;
+}
